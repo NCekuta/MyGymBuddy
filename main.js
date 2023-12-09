7d00898e42c1c240c612c1c8f83dd781efb317b6
NCekuta <nejc.cekuta@gmail.com>
2023-12-09 15:32:01 +0100

    Basic working messages tab

--- main.js
+++ main.js
@@ -1,37 +1,4 @@
-// animacije med login in register
-// linkamo vse gumbe in container
-const container = document.getElementById('container');
-const registerBtn = document.getElementById('register');
-const loginBtn = document.getElementById('login');
-// ob pritisku na gumb se doda/izbrise class active
-registerBtn.addEventListener('click', () => {container.classList.add("active")});
-loginBtn.addEventListener('click', () => {container.classList.remove("active")});
-
-
-// Funkcionalnost, da se login odpre ob pritisku na gumb login
-// linkamo login gumb in login form
-const openLoginBtn = document.getElementById('openLogin');
-const loginForm = document.getElementById('loginForm');
-
-// Ko kliknemo na login gumb se prikaže login
-openLoginBtn.addEventListener('click', () => {
-  loginForm.style.display = 'flex';
-});
-
-// Če kliknemo izven login okna se okno zapre
-window.addEventListener('click', (event) => {
-  if (event.target === loginForm) {
-    loginForm.style.display = 'none';
-  }
-});
-
-//zapremo s kiščkom
-loginCloseIcon = document.getElementById('login-close-icon');
-loginCloseIcon.addEventListener('click', () => {
-  loginForm.style.display = 'none';
-});
-
-/*dark/light mode ikona*/
+//dark/light mode ikona -------------------------------------------------------------------------------------------------------------------
 // pogledamo uporabnikovo željeno temo v local storage
 const isLightTheme = localStorage.getItem("theme") === "light";
 
@@ -60,23 +27,41 @@ moon_sun.onclick = function(){
   localStorage.setItem("theme", theme);
 }
 
-// kontakt, doda .focus class da se labeli premaknejo gor in tm ostanejo če je kej not napisano
-const contactInputs = document.querySelectorAll(".contact-input");
-function focusFunc(){
-  let parent = this.parentNode;
-  parent.classList.add("focus");
-}
-function blurFunc(){
-  let parent = this.parentNode;
-  if(this.value == ""){
-  parent.classList.remove("focus");
-  }
-}
 
-contactInputs.forEach((input) => {
-  input.addEventListener("focus", focusFunc);
-  input.addEventListener("blur", blurFunc);
-})
+// login/registration -----------------------------------------------------------------------------------------------------
+
+// animacije med login in register
+// linkamo vse gumbe in container
+const container = document.getElementById('container');
+const registerBtn = document.getElementById('register');
+const loginBtn = document.getElementById('login');
+// ob pritisku na gumb se doda/izbrise class active
+registerBtn.addEventListener('click', () => {container.classList.add("active")});
+loginBtn.addEventListener('click', () => {container.classList.remove("active")});
+
+
+// Funkcionalnost, da se login odpre ob pritisku na gumb login
+// linkamo login gumb in login form
+const openLoginBtn = document.getElementById('openLogin');
+const loginForm = document.getElementById('loginForm');
+
+// Ko kliknemo na login gumb se prikaže login
+openLoginBtn.addEventListener('click', () => {
+  loginForm.style.display = 'flex';
+});
+
+// Če kliknemo izven login okna se okno zapre
+window.addEventListener('click', (event) => {
+  if (event.target === loginForm) {
+    loginForm.style.display = 'none';
+  }
+});
+
+//zapremo z iksom
+loginCloseIcon = document.getElementById('login-close-icon');
+loginCloseIcon.addEventListener('click', () => {
+  loginForm.style.display = 'none';
+});
 
 // Login eye icon
 function togglePasswordVisibility(icon, formType) {
@@ -145,7 +130,26 @@ function checkPasswordStrength(password) {
   }
 }
 
+// contact tab --------------------------------------------------------------------------------------------------------------------
+// kontakt, doda .focus class da se labeli premaknejo gor in tm ostanejo če je kej not napisano
+const contactInputs = document.querySelectorAll(".contact-input");
+function focusFunc(){
+  let parent = this.parentNode;
+  parent.classList.add("focus");
+}
+function blurFunc(){
+  let parent = this.parentNode;
+  if(this.value == ""){
+  parent.classList.remove("focus");
+  }
+}
 
+contactInputs.forEach((input) => {
+  input.addEventListener("focus", focusFunc);
+  input.addEventListener("blur", blurFunc);
+})
+
+// omogočeno pošiljanje sporočil na mail
 const contactForm = document.getElementById("form-contact");
 const contactFirstName = document.getElementById("contact_firstName");
 const contactLastName = document.getElementById("contact_lastName");
@@ -216,3 +220,48 @@ contactForm.addEventListener("submit", (e) => {
     attachmentList.innerHTML = '';
     return false;
 })
+
+// messages tab ------------------------------------------------------------------------------------------------------------
+// Function to populate messages
+function populateMessages() {
+  // Sample messages data
+  const messagesData = [
+    { id: 1, subject: 'Message 1', content: 'Content of Message 1' },
+    { id: 2, subject: 'Message 2', content: 'Content of Message 2' },
+    { id: 3, subject: 'Message 3', content: 'Content of Message 3' },
+    // Add more messages as needed
+  ];
+
+  // Select elements
+  const sidebar = document.getElementById('sidebar');
+  const mainContent = document.getElementById('main-content');
+
+  // Populate sidebar with messages
+  const messagesList = document.createElement('ul');
+  messagesData.forEach(message => {
+    const listItem = document.createElement('li');
+    listItem.textContent = message.subject;
+    listItem.addEventListener('click', () => displayMessage(message));
+    messagesList.appendChild(listItem);
+  });
+  sidebar.appendChild(messagesList);
+
+  // Function to display message in the main content area
+  function displayMessage(message) {
+    mainContent.innerHTML = `
+      <h2>${message.subject}</h2>
+      <p>${message.content}</p>
+      <button onclick="replyToMessage(${message.id})">Reply</button>
+      <button onclick="forwardMessage(${message.id})">Forward</button>
+    `;
+  }
+
+  // Sample functions for replying and forwarding messages
+  window.replyToMessage = function (messageId) {
+    alert(`Replying to message with ID ${messageId}`);
+  };
+
+  window.forwardMessage = function (messageId) {
+    alert(`Forwarding message with ID ${messageId}`);
+  };
+}
