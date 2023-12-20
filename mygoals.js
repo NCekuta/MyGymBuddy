d6b3b5194a6d28a6d030de4b732bdae9c77cbd32
JanNamestnik <104309844+JanNamestnik@users.noreply.github.com>
2023-12-20 13:49:01 +0100

    todo list

--- mygoals.js
+++ mygoals.js
@@ -7,20 +7,20 @@ const countValue = document.querySelector(".count-value")
 let taskCount = 0;
 
 const displayCount = (taskCount) => {
-  countValue.innerText = taskCount;
+    countValue.innerText = taskCount;
 }
 
 const addTask = () => { // dodajanje taskov
     const taskName = newTaskInput.value.trim();
     error.style.display = "none";
-    if(!taskName){
-        setTimeout(() =>{
-        error.style.display = "block";
+    if (!taskName) {
+        setTimeout(() => {
+            error.style.display = "block";
         }, 200);
         return;
     }
 
-const task = `<div class="task">
+    const task = `<div class="task">
 <input type="checkbox" class="task-check">
 <span class="taskname">${taskName}</span>
 <button class="edit">
@@ -30,32 +30,52 @@ const task = `<div class="task">
 <i class="fa-solid fa-trash"></i>
 </button>
 </div>`;
-tasksContainer.insertAdjacentHTML
-("beforeend", task);
+    tasksContainer.insertAdjacentHTML
+        ("beforeend", task);
 
-const deleteButtons = document.querySelectorAll(".delete");  // brisanje taskov
-deleteButtons.forEach((button) => {
-button.onclick = () => {
-button.parentNode.remove();
-taskCount = -1;
-displayCount(taskCount);
-};
-});
+    const deleteButtons = document.querySelectorAll(".delete");  // brisanje taskov
+    deleteButtons.forEach((button) => {
+        button.onclick = () => {
+            button.parentNode.remove();
+            taskCount -= 1;
+            displayCount(taskCount);
+        };
+    });
 
-const editButtons = document.querySelectorAll(".edit"); //spreminjanje taskov
-editButtons.forEach((editBtn) => {
-    editBtn.onclick = (e) => {
-     let targetElement = e.target;
-     if(!(e.target.className == "edit")){
-        targetElement = e.target.parentElement;
-     }
-     newTaskInput.value = targetElement.previousElementSibling?.innerText;
-     targetElement.parentNode.remove();
-     taskCount -= 1;
-     displayCount(taskCount);
-    };
-});
-
+    const editButtons = document.querySelectorAll(".edit"); //spreminjanje taskov
+    editButtons.forEach((editBtn) => {
+        editBtn.onclick = (e) => {
+            let targetElement = e.target;
+            if (!(e.target.className == "edit")) {
+                targetElement = e.target.parentElement;
+            }
+            newTaskInput.value = targetElement.previousElementSibling?.innerText;
+            targetElement.parentNode.remove();
+            taskCount -= 1;
+            displayCount(taskCount);
+        };
+    });
+    const tasksCheck = document.querySelectorAll(".task-check") //opravljeni taski
+    tasksCheck.forEach((checkBox) => {
+        checkBox.onchange = () => {
+            checkBox.nextElementSibling.classList.toggle("completed");
+            if (checkBox.checked) {
+                taskCount -= 1;
+            }
+            else {
+                taskCount += 1;
+            }
+            displayCount(taskCount);
+        };
+    });
+    taskCount += 1;
+    displayCount(taskCount);
+    newTaskInput.value = "";
 };
 
 addBtn.addEventListener("click", addTask);
+    window.onload =  () => {
+    taskCount = 0;
+    displayCount(taskCount);
+    newTaskInput.value = "";
+}   
