cd0ec53563c4d82dea8d8933ac8742f4bd065960
Jure Vajs <91623771+jurevajs@users.noreply.github.com>
2024-01-19 22:05:17 +0100

    password change

--- src/index.js
+++ src/index.js
@@ -2,9 +2,12 @@ const express = require('express');
 const path = require('path');
 const bcrypt = require('bcrypt');
 const collection = require("./config");
+const session = require('express-session');
 
 const app = express();
 app.use(express.json());
+
+app.use(session({ secret: 'your-secret-key', resave: true, saveUninitialized: true }));
 
 app.use(express.static("public"));
 
@@ -18,6 +21,7 @@ app.get("/", (req, res) =>{
 })
 
 app.get("/login", (req, res) =>{
+    req.session.returnTo = req.header('Referer') || '/';
     res.render('login');
 })
 
@@ -63,13 +67,53 @@ app.post("/login", async (req, res) => {
             res.send("wrong Password");
         }
         else {
-            res.redirect(`/main.html?Ime=${check.Ime}`);
+            const returnTo = req.session.returnTo || '/';
+            delete req.session.returnTo;
+            req.session.Ime = check.Ime;
+            res.redirect(`${returnTo}?Ime=${check.Ime}`);
         }
     }
     catch {
         res.send("wrong Details");
     }
 })
+
+app.get("/change-password", (req, res) => {
+    res.render("change-password");
+});
+
+app.post("/change-password", async (req, res) => {
+    const { username, oldPassword, newPassword, confirmPassword } = req.body;
+
+    if (newPassword !== confirmPassword) {
+        return res.send("New password and confirm password do not match");
+    }
+
+    try {
+        const existingUser = await collection.findOne({ Ime: username });
+
+        if (!existingUser) {
+            return res.send("User not found");
+        }
+
+        const isPasswordMatch = await bcrypt.compare(oldPassword, existingUser.Geslo);
+
+        if (!isPasswordMatch) {
+            return res.send("Incorrect old password");
+        }
+
+        const saltRounds = 10;
+        const hashedNewPassword = await bcrypt.hash(newPassword, saltRounds);
+
+        await collection.updateOne({ Ime: username }, { $set: { Geslo: hashedNewPassword } });
+
+        res.redirect('main.html');
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Internal Server Error");
+    }
+});
+
 
 const port = 5001;
 app.listen(port, () => {
